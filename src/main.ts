31de0be1a1c0d35ed3142979e9975dbb34ee8556
DanielFco <ite.danielbanda@gmail.com>
2023-09-19 12:57:32 -0600

    Configuration and .env file added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule, { cors: true });
@@ -13,6 +15,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('', app, document);
   app.enableCors();
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: configService.get('auth.host'),
+      port: configService.get('auth.rpcPort')
+    }
+  });
 }
+
 bootstrap();
