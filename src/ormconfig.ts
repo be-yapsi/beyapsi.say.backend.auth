0f14a49057dd8495c5a932b7b7589f87fc828ee3
LauraOliviaSS <testingoli26@gmail.com>
2023-10-13 13:00:24 -0600

    synchronize disabled

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -19,7 +19,7 @@ export const ormconfig: TypeOrmModuleOptions = {
   entities: [
     "C:/Projects/BeYapsi/libraries/beyapsi.yapsi.backend.sqlserver/dist/entities/*.entity{.ts,.js}"
   ],
-  synchronize: true,
+  synchronize: false,
   autoLoadEntities: true,
   logging: true,
   options: {
